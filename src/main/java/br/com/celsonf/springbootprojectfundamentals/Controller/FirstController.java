ed627185cdac52eef14e4bfa0546809cbc690593
Celson Fernando <29209254+CelsonF@users.noreply.github.com>
2024-02-07 17:37:32 +0000

    feat:Create controller using pathVariables

--- src/main/java/br/com/celsonf/springbootprojectfundamentals/Controller/FirstController.java
+++ src/main/java/br/com/celsonf/springbootprojectfundamentals/Controller/FirstController.java
@@ -3,16 +3,23 @@ package br.com.celsonf.springbootprojectfundamentals.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
 @RequestMapping("/firstController")
 public class FirstController {
     
-    @GetMapping("/")
-    public String getMethodName() {
-        System.out.println("Estou funcionando");
-        return "Estou funcionando";
+    @GetMapping("/firstMethod/{id}")
+    public String firstMethod(@PathVariable String id) {
+        return "This response is using PathVariables" + id;
     }
+
+    @GetMapping("/secondMethod/{name}")
+    public String secondMethod(@PathVariable(name = "id") String id) {
+        return "This response is using PathVariables and named item in path variable." + id;
+    }
+
+
     
 }
