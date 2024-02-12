362306ec840f77c5cc078b9178a6f6f93cc7431a
CelsonF <celsondev@outlook.com>
2024-02-12 01:18:31 -0300

    feat: add request body and params

--- src/main/java/br/com/celsonf/springbootprojectfundamentals/Controller/FirstController.java
+++ src/main/java/br/com/celsonf/springbootprojectfundamentals/Controller/FirstController.java
@@ -1,9 +1,16 @@
 package br.com.celsonf.springbootprojectfundamentals.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -12,14 +19,30 @@ public class FirstController {
     
     @GetMapping("/firstMethod/{id}")
     public String firstMethod(@PathVariable String id) {
-        return "This response is using PathVariables" + id;
+        return "This response is using PathVariables " + id;
     }
 
-    @GetMapping("/secondMethod/{name}")
-    public String secondMethod(@PathVariable(name = "id") String id) {
-        return "This response is using PathVariables and named item in path variable." + id;
+    @GetMapping("/methodWithQueryParams")
+    public String methodWithQueryParams(@RequestParam  String id ) {
+        return "The params with method with query params " + id;
     }
 
+    @GetMapping("/methodWithQueryParamsMap")
+    public String methodWithQueryParams(@RequestParam Map<String, String> allParams) {
+        return "The params with method with query params and using Map " + allParams.entrySet();
+    }
 
+    @PostMapping("/methodWithBodyParams")
+    public String methodWithBodyParams(@RequestBody Usuario usuario) {
+        
+        return "methodWithBodyParams " + usuario.username();
+    }
+    
+
+    /**
+     * Usuario
+String username     */
+    public record Usuario(String username) {
+    }
     
 }
